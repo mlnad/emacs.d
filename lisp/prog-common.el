38b48cd75257438e4b5c7c899756ae915fb3f0d4
Liu Miao <liumiaogemini@foxmail.com>
2020-09-28 11:57:35 +0000

    Make a switch case between nox and lsp-mode

--- lisp/prog-common.el
+++ lisp/prog-common.el
@@ -6,34 +6,38 @@
 ;;;
 
 ;;; Language server protocol
-(use-package nox
-  :load-path "lisp/nox"
-  :config
-  (add-to-list 'nox-server-programs
-	       `(python-mode . ("pyls" "-v" "--tcp" "--host"
-				"localhost" "--port" :autoport)))
-  (dolist (hook (list
-		 'python-mode-hook
-		 'c-mode-hook
-		 'c-mode-common-hook
-		 'c++-mode-hook
-		 'haskell-mode-hook))
-    (add-hook hook '(lambda () (nox-ensure)))))
-
-;; (use-package lsp-mode
-;;   :ensure t
-;;   :init
-;;   (setq lsp-keymap-prefix "C-c l")
-;;   :hook (
-;; 	 (c-mode . lsp-deferred)
-;; 	 (c++-mode . lsp-deferred)
-;; 	 (python-mode . lsp-deferred)
-;; 	 (lsp-mode . lsp-enable-which-key-integration)
-;; 	 )
-;;   :config
-;;   (setq lsp-enable-snippet nil)
-;;   :commands (lsp lsp-deferred)
-;;   )
+(cond
+ ((eq 'nox user/lsp-client)
+  (use-package nox
+    :load-path "lisp/nox"
+    :config
+    (add-to-list 'nox-server-programs
+		 `(python-mode . ("pyls" "-v" "--tcp" "--host"
+				  "localhost" "--port" :autoport)))
+    (dolist (hook (list
+		   'python-mode-hook
+		   'c-mode-hook
+		   'c-mode-common-hook
+		   'c++-mode-hook
+		   'haskell-mode-hook))
+      (add-hook hook '(lambda () (nox-ensure)))))
+  )
+ ((eq 'lsp-mode user/lsp-client)
+  (use-package lsp-mode
+    :ensure t
+    :init
+    (setq lsp-keymap-prefix "C-c l")
+    :hook (
+	   (c-mode . lsp-deferred)
+	   (c++-mode . lsp-deferred)
+	   (python-mode . lsp-deferred)
+	   (lsp-mode . lsp-enable-which-key-integration)
+	   )
+    :config
+    (setq lsp-enable-snippet nil)
+    :commands (lsp lsp-deferred)
+    )
+  ))
 
 (use-package lsp-ui
   :ensure t
