edbaf48f31199a079582b46b66226bdca1be8cc0
Liu Miao <liumiaogemini@foxmail.com>
2021-06-26 23:21:09 +0800

    add file binding to bison-mode

--- lisp/prog-c-cpp.el
+++ lisp/prog-c-cpp.el
@@ -15,7 +15,10 @@
   (add-to-list 'company-backends 'company-c-headers))
 
 (use-package bison-mode
-  :ensure t)
+  :ensure t
+  :mode (("\\.lex\\'" . flex-mode)
+         ("\\.y\\'" . bison-mode)
+         ("\\.grm\\'" . bison-mode)))
 
 (provide 'prog-c-cpp)
 ;;; prog-c-cpp.el ends here
