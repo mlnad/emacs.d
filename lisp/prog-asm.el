f6a27fbc229ac4fb4801cbc4774612edb7f1f201
Liu Miao <liumiaogemini@foxmail.com>
2021-07-13 21:19:07 +0800

    add `prog-asm.el' for assembly programming.

---
+++ lisp/prog-asm.el
@@ -0,0 +1,10 @@
+;;; prog-asm.el --- Configuration for assembly code.
+;;; Commentary:
+
+;;; Code:
+(use-package nasm-mode
+  :ensure t
+  :mode "\\.nasm\\'")
+
+(provide 'prog-asm)
+;;; prog-asm.el ends here
