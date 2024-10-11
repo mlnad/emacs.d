4251bd0d37126d48e40acae836628549c376ca2b
Liu Miao <liumiaogemini@mail.ustc.edu.cn>
2024-10-11 20:51:22 +0800

    Basic command line support

---
+++ bin/moyu.el
@@ -0,0 +1,24 @@
+;;; moyu.el --- Command Line Supports -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Liu
+;;
+;; Author: Liu <liumiaogemini@foxmail.com>
+;; Maintainer: Liu <liumiaogemini@foxmail.com>
+;; Created: 十月 11, 2024
+;; Modified: 十月 11, 2024
+;; Version: 0.0.1
+;; Keywords: Symbol’s value as variable is void: finder-known-keywords
+;; Homepage:
+;; Package-Requires: ((emacs "28.1"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Command Line Supports for Emacs
+;;
+;;; Code:
+
+
+(provide 'moyu)
+;;; moyu.el ends here
